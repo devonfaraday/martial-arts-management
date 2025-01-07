fc47eaa0c4621ba053e4ec9e583acabd0bf50376
devonfaraday <christianmcmullin@gmail.com>
2025-01-07 00:08:27 -0700

    removed files I'm not going to use for a long time.

--- src/main/java/com/whitelabel/martialarts/repository/LocationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.whitelabel.martialarts.repository;
-
-import com.whitelabel.martialarts.model.Location;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
--- src/main/java/com/whitelabel/martialarts/repository/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.whitelabel.martialarts.repository;
-
-import com.whitelabel.martialarts.model.School;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SchoolRepository extends JpaRepository<School, Long> {
-}
